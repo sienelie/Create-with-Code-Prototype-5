e74689eb3b75069b940d0234d6a726341c6dc2f1
Chrissie Ugay <chrissie.anne.k.ugay@accenture.com>
2022-07-23 19:57:08 +0800

    added script to randomly generate and toss an object into the air on intervals; objects are givenr andom speed, position and torque; clicked object is destroyed;

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,16 +5,53 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float torque = 10.0f;
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float xRange = 4.0f;
+    private float ySpawnPos = -2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Destroy targets on mouse click 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    // Destroy targets when they are no longer visible in the screen
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-torque, torque);
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
